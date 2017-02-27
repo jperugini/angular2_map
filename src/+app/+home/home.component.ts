acc37fa2381e2a271a089377eed04df0271e9399
JPerugini <julien.perugini@gmail.com>
2017-02-27 18:48:39 +0700

    Added google map api

--- src/+app/+home/home.component.ts
+++ src/+app/+home/home.component.ts
@@ -11,12 +11,43 @@ import { ModelService } from '../shared/model/model.service';
 })
 export class HomeComponent {
     filesToUpload: File;
-    error = null;
-    uploadedJsonFile = null;
+    error: string;
+    uploadedJsonFile: any;
+    uploadFilename: string;
+    markers: Array<any>;
+    currentLat: number;
+    currentLong: number;
+    positionSupported: boolean;
+    initzoom: number;
+    selectedMarker: any;
 
 
     constructor() {
         this.filesToUpload = null;
+        this.uploadFilename = null;
+        this.error = null;
+        this.uploadedJsonFile = null;
+        this.markers = [];
+        this.positionSupported = false;
+        this.currentLat = 0;
+        this.currentLong = 0;
+        this.initzoom = 1;
+        this.selectedMarker = null;
+    }
+
+    getLocation(){
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(this.setPosition.bind(this));
+            this.positionSupported = true;
+        } else { 
+            this.error = "Geolocation is not supported by this browser.";
+        }
+    }
+
+    setPosition(position) {
+        this.currentLat = position.coords.latitude;
+        this.currentLong = position.coords.longitude;
+        this.initzoom = 11;
     }
 
     upload() {
@@ -26,7 +57,19 @@ export class HomeComponent {
             this.makeFileRequest("http://localhost:3000/api/upload", [], this.filesToUpload).then((result) => {
                 try {
                     this.uploadedJsonFile = JSON.parse(result.toString());
+                    if(!this.uploadedJsonFile.markers) {
+                        this.error = "JSON should have one array object markers";
+                        this.uploadedJsonFile = null;
+                    } else {
+                        this.markers = this.uploadedJsonFile.markers;
+                        this.markers.forEach((marker, i) => marker.label = i.toString());
+                        this.initzoom = 11;
+                        this.currentLat = this.markers[0].latitude;
+                        this.currentLong = this.markers[0].longitude;
+                        this.selectedMarker = this.markers[0];
+                    }
                 } catch (e) {
+                    console.log(e);
                     this.error = "JSON is not valid";
                 }
             }, (error) => {
@@ -38,10 +81,16 @@ export class HomeComponent {
     }
 
     fileChangeEvent(fileInput: any) {
-        if (fileInput.target.files[0].name.endsWith('.json')) {
-            this.filesToUpload = fileInput.target.files[0];
-        } else {
-            this.error = "Upload must be json";
+        if(fileInput.target.files.length > 0) {
+            // Reset error first
+            this.error = null;
+            // Put the filename in variable to display it
+            this.uploadFilename = fileInput.target.files[0].name;
+            if (this.uploadFilename.endsWith('.json')) {
+                this.filesToUpload = fileInput.target.files[0];
+            } else {
+                this.error = "Upload must be json";
+            }
         }
     }
 
@@ -62,6 +111,17 @@ export class HomeComponent {
             xhr.open("POST", url, true);
             xhr.send(formData);
         });
+    }
+
+    mapClicked($event: MouseEvent) {
+        this.markers.push({
+            latitutde: $event['coords'].lat,
+            longitude: $event['coords'].lng
+        });
+    }
+
+    clickedMarker(marker, i) {
+        this.selectedMarker = marker;
     }
 
 
--- src/+app/+home/home.module.ts
+++ src/+app/+home/home.module.ts
@@ -4,12 +4,16 @@ import { SharedModule } from '../shared/shared.module';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { AgmCoreModule } from 'angular2-google-maps/core';
 
 @NgModule({
   imports: [
     SharedModule,
     HomeRoutingModule,
-    HttpModule
+    HttpModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAjMGmvoOK4RfMPVO-52rt87CEfhR2TUaw'
+    })
   ],
   declarations: [
     HomeComponent
